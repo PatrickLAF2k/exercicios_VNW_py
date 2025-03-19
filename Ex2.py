6ecf41ec104677655e02c93c477abd7501f6849b
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-03-19 14:00:44 -0300

    Concluido exercicio 2

--- Ex2.py
+++ Ex2.py
@@ -1,6 +1,12 @@
 # Segundo Exercício: Ordenação de Tuplas
 # Dada uma lista de tuplas, onde cada tupla contém o nome de uma pessoa e sua idade,
 # escreva uma função que retorne a lista ordenada pela idade de forma crescente.
-# Exemplo: (“Samuel”, ‘Karynne”, “Carol”, “Kleber”, “Vinicius”)
+Exemplo = ("Samuel", "Karynne", "Carol", "Kleber", "Vinicius");
 # Saída: (“Carol”, “Karynne”, “Kleber”, “Samuel”, “Vinicius”)
+
+def tuplas_ordenadas (a):
+    ordenado = sorted(a)
+    return ordenado
+
+print(tuplas_ordenadas(Exemplo))
 
