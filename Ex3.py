56d19632b5ed1dfcba5175f04725fdcbb708f0cc
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-03-21 16:11:59 -0300

    Concluido ex3

--- Ex3.py
+++ Ex3.py
@@ -1,5 +1,13 @@
 # Terceiro Exercício: Contagem de Frequência
 # Escreva uma função que recebe uma string e retorna um dicionário onde as chaves são os
 # caracteres da string e os valores representam quantas vezes cada caractere aparece.
-# Exemplo: [‘Java’, ‘Java’, ‘Ruby’, ‘Javascript’, ‘Ruby’]
+Exemplo = ["Java", "Java", "Ruby", "Javascript", "Ruby"];
 # Saída: {‘Java’: 2, ‘Ruby’: 2, ‘Javascript’: 1}
+
+def contagem_ferquencia (a):
+    frequencia = {}
+    for i in a:
+        frequencia[i] = frequencia.get(i, 0) + 1
+    return frequencia
+
+print(contagem_ferquencia(Exemplo))
