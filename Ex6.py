f38e09d08801b7dd66a364ae3b5e110dc3c90846
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-03-21 20:00:42 -0300

    Concluido ex 6 ese preciseo de ajuda do copilot

--- Ex6.py
+++ Ex6.py
@@ -3,6 +3,15 @@
 # inteiros. A função deve combinar os dicionários somando os valores das chaves que
 # aparecem em ambos.
 # Exemplo:
-# d1 = {"a": 2, "b": 3, "c": 5}
-# d2 = {"a": 1, "b": 4, "d": 7}
+d1 = {"a": 2, "b": 3, "c": 5}
+d2 = {"a": 1, "b": 4, "d": 7}
 # Saída: {"a": 3, "b": 7, "c": 5, "d": 7}
+
+def soma_dicionarios (a, b):
+    dicionario = a
+    
+    for key, valor in b.items():
+        a[key] = a.get(key, 0) + valor
+    return dicionario
+
+print(soma_dicionarios(d1, d2))
