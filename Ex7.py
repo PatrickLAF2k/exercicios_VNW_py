1fdae1d578f2a50c84076d940f45f4e65e8cec60
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-03-21 20:07:21 -0300

    concluido ex 7 e comentado pois nao entendi

--- Ex7.py
+++ Ex7.py
@@ -1,5 +1,21 @@
 # Sétimo Exercício: Top 3 mais frequentes
 # Dada uma lista de números, crie uma função que retorne os três números mais frequentes
 # em ordem decrescente de frequência. Se houver empates, ordene pelo valor numérico.
-# Exemplo: [1, 3, 3, 2, 1, 1, 4, 4, 4, 2, 2, 2, 5, 5]
+Exemplo = [1, 3, 3, 2, 1, 1, 4, 4, 4, 2, 2, 2, 5, 5]
 # Saída: [2, 1, 4]
+
+def frequentes(a):
+    contagem = {}  # Inicializa o dicionário para contar as ocorrências
+
+    for i in a:
+        if i in contagem:  # Se a chave já existe, soma 1
+            contagem[i] += 1
+        else:  # Se a chave não existe, inicializa com 1
+            contagem[i] = 1
+
+    # Ordena primeiro por frequência (decrescente) e depois pelo número (crescente)
+    mais_frequentes = sorted(contagem.keys(), key=lambda x: (-contagem[x], x))
+
+    return mais_frequentes[:3]  # Retorna os 3 mais frequentes
+
+print(frequentes(Exemplo))
