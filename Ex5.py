d1f93da513185d856ef193c2480f6728407c55a8
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-03-21 16:31:13 -0300

    Concluido ex 5

--- Ex5.py
+++ Ex5.py
@@ -2,5 +2,17 @@
 # Dado um dicionário onde as chaves são nomes de alunos e os valores são listas com suas
 # notas, crie uma função que retorna uma lista de tuplas, onde cada tupla contém o nome do
 # aluno e sua média de notas.
-# Exemplo: {"Ana": [8, 9, 7], "Bruno": [5, 6, 5], "Carlos": [10, 9, 10]}
+Exemplo = {"Ana": [8, 9, 7], "Bruno": [5, 6, 5], "Carlos": [10, 9, 10]}
 # Saída: [("Ana", 8.0), ("Bruno", 5.33), ("Carlos", 9.67)]
+
+def medias (a):
+    lista = []
+    
+    for i in a:
+        media = sum(a[i]) / len(a[i])
+        lista.append((i, round(media,2)))
+    return lista
+
+print(medias(Exemplo))
+    
+    
